cedc3abf2f5abf7aac4b7d0671cc661f6ac848bc
Siddhartha Srivastava <110695015+Sid62@users.noreply.github.com>
2023-10-04 00:01:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import products from "./db/data";
+
+import Card from "./components/Card";
+import "./index.css";
+
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Applying selected filter
+   
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(products, selectedCategory, query);
+
+  return (
+    <>
+     
+      <Navigation query={query} handleInputChange={handleInputChange} />
+
+      <Products result={result} />
+    </>
+  );
+}
+
+export default App;
